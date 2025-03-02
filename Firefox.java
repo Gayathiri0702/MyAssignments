3dddd817d3e680b6209f0190dfa86b15d6a0616d
Gayathiri0702 <gayathiri0702@gmail.com>
2025-03-02 21:08:03 +0530

    Add files via upload
    Week1 day1 : Firefox

---
+++ Firefox.java
@@ -0,0 +1,16 @@
+package week1day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserversion=100.2f;
+		String browsername="firefox";
+		boolean isvisible=true;
+		int releaseYear = 1998;
+		char broserlogo='f';
+		System.out.println("BrowserVersion is:"+browserversion+","+"BrowserName is:"+browsername+","+"ISVisible:"+isvisible+","+"releaseYear:"+releaseYear+"),"+"BrandLogo:"+"brandlogo+");
+
+	}
+	
+}
