db0808b05d54a60bd3eaa7ecd2cc498e4f105437
Gayathiri0702 <gayathiri0702@gmail.com>
2025-03-04 13:56:47 +0530

    Add files via upload
    odd numbers

---
+++ Oddnumbers.java
@@ -0,0 +1,18 @@
+package week1day2;
+
+public class Oddnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    for (int i=1; i<=10; i++) {
+    if(i%2!=0) {
+    System.out.println(i);
+    
+    	
+    	
+    }
+    }
+    
+	}
+
+}
