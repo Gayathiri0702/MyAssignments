09e25900b3c77c69975fd7b1fe6ac0dfc3ead17c
Gayathiri0702 <gayathiri0702@gmail.com>
2025-03-04 13:33:45 +0530

    Add files via upload
    week1day1(chrome)

---
+++ chrome.java
@@ -0,0 +1,21 @@
+package week1day1;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+        float ChromeVersion=91.0f;
+		String ChromeDeveloper="Google";
+		boolean isBetaVersion= false;
+	    int ReleaseYear= 2008;
+		char shortcutkey= 'c';
+		System.out.println("ChromeVersion is:"+ChromeVersion+'\n'+"ChromeDeveloper is:"+ChromeDeveloper+'\n'+"isBetaVersion:"+isBetaVersion+'\n'+"ReleaseYear:"+ReleaseYear+'\n'+"ShortcutKey:"+shortcutkey);
+				
+		
+		
+				
+
+	}
+
+}
